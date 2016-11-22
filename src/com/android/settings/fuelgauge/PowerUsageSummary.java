dd4c5d7ba8e47c6ccc7a57383a8add9a9b8eb0cc
myfluxi <linflux@arcor.de>
2016-11-22 21:06:15 +0100

    Settings: skip Misc/Overcounted battery stats in builds
    Quadruple battery lifez.
    
    Change-Id: I6f829dd62c8820a38f2ac1301e0fdc345852d1d3

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -446,7 +446,7 @@ public class PowerUsageSummary extends PowerUsageBase {
                     if (percentOfTotal < 10) {
                         continue;
                     }
-                    if ("user".equals(Build.TYPE)) {
+                    if ("user".equals(Build.TYPE) || "userdebug".equals(Build.TYPE)) {
                         continue;
                     }
                 }
@@ -459,7 +459,7 @@ public class PowerUsageSummary extends PowerUsageBase {
                     if (percentOfTotal < 5) {
                         continue;
                     }
-                    if ("user".equals(Build.TYPE)) {
+                    if ("user".equals(Build.TYPE) || "userdebug".equals(Build.TYPE)) {
                         continue;
                     }
                 }
